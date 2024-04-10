531343ca3e1dc6599f00927997581c53f5098940
Sam20020618 <sam.smith@students.mq.edu.au>
2024-04-10 14:01:28 +1000

    added movement

--- Assets/Scrpits/MoveStar.cs
+++ Assets/Scrpits/MoveStar.cs
@@ -11,7 +11,10 @@ using Liminal.SDK.VR.Input;
 public class MoveStar : MonoBehaviour
 {
     public Transform Cube;
+    public float Timelock;
+    private float Timeset = 1.0f;
     public Transform Player;
+    private Vector3 Lastpos;
     private bool Trigger;
    private bool Clicktrigger;
    public bool ClickTrigger 
@@ -32,6 +35,7 @@ public class MoveStar : MonoBehaviour
     {
       Trigger = false;
       Clicktrigger = false;
+      Timelock = Timeset;
     }
 
     // Update is called once per frame
@@ -50,6 +54,35 @@ public class MoveStar : MonoBehaviour
         {
           Trigger = true;
         }
+    }
+    if(Trigger)
+    {
+      if(Timelock <= 0)
+      {
+         Lastpos = Player.position;
+         Timelock = Timeset;
+      }
+       else {
+          Timelock -= Time.deltaTime;
+       }
+       Debug.Log(Lastpos);
+        Debug.Log(Player.position);
+       if(Lastpos.x < Player.position.x)
+       {
+         Debug.Log("Go left");
+         transform.Translate(Vector3.left * Time.deltaTime);
+       } else 
+       {
+         transform.Translate(Vector3.right * Time.deltaTime);
+       }
+      //   if(Lastpos.y < Player.position.y)
+      //  {
+      //    Debug.Log("Go left");
+      //    transform.Translate(Vector3.up * Time.deltaTime);
+      //  } else 
+      //  {
+      //    transform.Translate(Vector3.down * Time.deltaTime);
+      //  }
     }
   }
 
